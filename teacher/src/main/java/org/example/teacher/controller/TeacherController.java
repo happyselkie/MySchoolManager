8030f204bd4b44959cdd3a153e37273ff7ec554d
fbanier <frbanier@gmail.com>
2025-07-04 16:09:58 +0200

    change route for lisibility

--- teacher/src/main/java/org/example/teacher/controller/TeacherController.java
+++ teacher/src/main/java/org/example/teacher/controller/TeacherController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("teacher")
+@RequestMapping("api")
 public class TeacherController {
 
     private final TeacherService teacherService;
